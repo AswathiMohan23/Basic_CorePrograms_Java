79969b4983a5fd1571da9e9608ba708a8be265d7
AswathiMohan23 <achu999here@gmail.com>
2022-10-09 16:03:27 +0530

    [ADDED] : code to find the percentage of head and tail after flipping a coin

---
+++ src/com/java/FlipCoin.java
@@ -0,0 +1,44 @@
+package com.java;
+//Flip Coin and print percentage of Heads and Tails
+// I/P -> The number of times to Flip Coin. Ensure it is a positive integer.
+//Logic -> Use Random Function to get value between 0 and 1. If < 0.5 then tails or heads
+// O/P -> Percentage of Head vs Tails
+
+import java.util.Scanner;
+
+public class FlipCoin {
+    public static void main(String[] args){
+        double randomCheck = 0;
+        int head=0,tail=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("how many times do want to flip the coin : ");
+        int times=sc.nextInt();
+        int limit=times;
+        if(times==(int) times) {
+            for (int i = 0; i < times; i++) {
+                randomCheck = (int) (Math.random() * (1 - 0 + 1) + 0);
+
+                if (randomCheck < 0.5) {
+                    System.out.println("Head");
+                    head=head+1;
+                } else {
+                    System.out.println("Tail");
+                    tail=tail+1;
+                }
+            }
+        }
+        float percentage_Head=findPercentage(head,limit);
+                System.out.println("Percentage of occurrence of Head = "+percentage_Head+"%");
+                float percentage_Tail=findPercentage(tail,limit);
+                System.out.println("Percentage of occurrence of Tail = "+percentage_Tail+"%");
+
+
+    }
+    public static float findPercentage(int side,int times) {
+        float percentage=(float)(side*100)/times;
+        return percentage;
+        }
+    }
+
+
+
