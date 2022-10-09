1d054d1d100a47715ba20496a4984edad3d1c698
AswathiMohan23 <achu999here@gmail.com>
2022-10-09 16:57:15 +0530

    [ADDED] : a Java Program to Check Whether a Number is Even or Odd

---
+++ src/com/java/Even_Odd.java
@@ -0,0 +1,18 @@
+package com.java;
+
+// Java Program to Check Whether a Number is Even or Odd
+// =====================================================
+
+import java.util.Scanner;
+
+public class Even_Odd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int number = sc.nextInt();
+        if ((number % 2) == 0)
+            System.out.println("the entered no is ==> even");
+        else
+            System.out.println("the entered no is ==> odd");
+    }
+}
