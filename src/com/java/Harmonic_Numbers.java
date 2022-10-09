8ffe435c2c7e942d286ae98a91dd6ca024489a45
AswathiMohan23 <achu999here@gmail.com>
2022-10-09 17:43:04 +0530

    [ADDED] : a Java Program to find the nth harmonic number

---
+++ src/com/java/Harmonic_Numbers.java
@@ -0,0 +1,23 @@
+package com.java;
+
+//Desc -> Prints the Nth harmonic number: 1/1 + 1/2 + ... + 1/N
+//(http://users.encs.concordia.ca/~chvatal/notes/harmonic.html).
+//I/P -> The Harmonic Value N. Ensure N != 0
+//Logic -> compute 1/1 + 1/2 + 1/3 + ... + 1/N
+// O/P -> Print the Nth Harmonic Value.
+
+import java.util.Scanner;
+
+public class Harmonic_Numbers {
+    public static void main(String[] args) {
+        float sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 1st no : ");
+        int number=sc.nextInt();
+        for (int i = 2; i <= number; i++) {
+            float harmonic = (float) 1 / i;
+            sum = sum + harmonic;
+            System.out.println(sum);}
+        System.out.print("\nnth Harmonic number = " + sum+"\n");
+    }
+}
