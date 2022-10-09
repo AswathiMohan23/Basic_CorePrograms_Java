15234158a928d84a944c9729a06aa9050d2c164a
AswathiMohan23 <achu999here@gmail.com>
2022-10-09 16:52:25 +0530

    [ADDED] : a Java Program to Find the Largest Among Three Numbers

---
+++ src/com/java/LargestElement.java
@@ -0,0 +1,24 @@
+package com.java;
+
+import java.util.Scanner;
+
+// Java Program to Find the Largest Among Three Numbers
+// ===================================================
+public class LargestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 1st no : ");
+        int firstNo = sc.nextInt();
+        System.out.println("Enter the 2nd no : ");
+        int secondNo = sc.nextInt();
+        System.out.println("Enter the 3rd no : ");
+        int thirdNo = sc.nextInt();
+        if ((firstNo > secondNo) && (firstNo > thirdNo))
+            System.out.println("largest number = " + firstNo);
+        else if ((secondNo > firstNo) && (secondNo > thirdNo))
+            System.out.println("largest number = " + secondNo);
+        else
+            System.out.println("largest number = " + thirdNo);
+    }
+}
+
