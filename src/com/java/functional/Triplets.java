79d451297e02ef52cd22f2da4793f4b14e7744d7
AswathiMohan23 <achu999here@gmail.com>
2022-10-10 14:24:16 +0530

    [ADDED] : code to find the triplets among the given nos whose sum equals to zero

---
+++ src/com/java/functional/Triplets.java
@@ -0,0 +1,25 @@
+package com.java.functional;
+
+import java.util.Scanner;
+
+public class Triplets {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the size of the array : ");
+        int size=sc.nextInt();
+        int [] inputArray=new int[size];
+        System.out.println("Enter the elements : ");
+        for(int j=0;j<size;j++){
+            inputArray[j]=sc.nextInt();
+        }
+        for(int i=0;i<inputArray.length;i++){
+            for(int j=i+1;j<inputArray.length;j++){
+                for(int k=j+1;k<inputArray.length;k++){
+                    if(inputArray[i] + inputArray[j] + inputArray[k]==0){
+                        System.out.println("triplets are : \n"+inputArray[i]+"\n"+inputArray[j]+"\n"+inputArray[k]);
+                    }
+                }
+            }
+        }
+    }
+}
