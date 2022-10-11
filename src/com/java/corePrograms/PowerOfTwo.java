02f3b366b6f73526ee87dde20d184c636945ad42
AswathiMohan23 <achu999here@gmail.com>
2022-10-11 17:32:39 +0530

    [REFACTORED] : the code by adding a method to find the power of 2

--- src/com/java/corePrograms/PowerOfTwo.java
+++ src/com/java/corePrograms/PowerOfTwo.java
@@ -10,12 +10,15 @@ public class PowerOfTwo {
         System.out.println("enter the number : ");
         Scanner sc=new Scanner(System.in);
         int number=sc.nextInt();
+        findPowerOfTwo(number);
+    }
+    public static void findPowerOfTwo(int number) {
         if((number>=0)&&(number<31)){
             System.out.println("2^n = "+(int) Math.pow(2,number)+ "\nTable of power of 2 which is " +
                     "less than or equal to "+(int) Math.pow(2,number)+" : ");
             for(int i=0;i<=(int) Math.pow(2,number);i++){
                 System.out.println("2^"+i+" : " +(int) Math.pow(2,i));
-           }
+            }
         }
     }
 }
