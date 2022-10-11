8e9449c3e8dd8de1272da968d7bc07de25f80f20
AswathiMohan23 <achu999here@gmail.com>
2022-10-11 17:41:43 +0530

    [REFACTORED] : the code by adding a method to find the euclidean distance

--- src/com/java/functional/EuclideanDistance.java
+++ src/com/java/functional/EuclideanDistance.java
@@ -4,13 +4,16 @@ package com.java.functional;
 //formulae to calculate distance = sqrt(x*x + y*y). Use Math.power function
 import java.util.Scanner;
 
-public class Distance {
+public class EuclideanDistance {
     public static void main(String[] args) {
         System.out.println("enter the value of X co-ordinate : ");
         Scanner sc = new Scanner(System.in);
         int xCoordinate = sc.nextInt();
         System.out.println("enter the value of Y co-ordinate : ");
         int yCoordinate = sc.nextInt();
+        findDistance(xCoordinate,yCoordinate);
+    }
+    public static void findDistance(int xCoordinate, int yCoordinate) {
         int powerOfX= (int) Math.pow(xCoordinate,2);
         int powerOfY=(int) Math.pow(yCoordinate,2);
         double distance =Math.sqrt(powerOfX+powerOfY);
