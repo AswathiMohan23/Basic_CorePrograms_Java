f36c31063890a8b7d6409ddb5e82701c0274e721
AswathiMohan23 <achu999here@gmail.com>
2022-10-11 16:41:25 +0530

    [REFACTORED] : the condition of the wind chill code

--- src/com/java/functional/WindChill.java
+++ src/com/java/functional/WindChill.java
@@ -17,15 +17,15 @@ public class WindChill {
         double temperature = sc.nextDouble();
         System.out.println("enter the wind speed in miles per hour (speed>120 or speed <3) : ");
         double velocity = sc.nextDouble();
-        if((temperature<50)&&((velocity>120)||(velocity<3))){
+        if((temperature<50)&&((velocity<120)||(velocity>3))){
             double speed=Math.pow(velocity,0.16);
             double product=  ((0.4275*temperature) - 35.75);
             double windChill =35.74+(0.6215*temperature)+(product*speed);
             System.out.println("Wind chill ,W=35.74+0.6215t+(0.4275t-35.75)v^0.16  ");
             System.out.println("result = "+windChill);
         }else
-            System.out.println("temperature  should be larger than 50 \n " +
-                    "speed should be larger than 120 and less than 3");
+            System.out.println("temperature  should not be larger than 50 \n " +
+                    "speed should be less than 120 and greater than 3");
 
     }
 }
