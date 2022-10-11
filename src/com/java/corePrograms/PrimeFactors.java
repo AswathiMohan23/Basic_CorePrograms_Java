0f1242f322f760dc232dba66da3e46cb8ba3f49a
AswathiMohan23 <achu999here@gmail.com>
2022-10-11 17:09:32 +0530

    [REFACTORED] : the class name of the code

--- src/com/java/corePrograms/PrimeFactors.java
+++ src/com/java/corePrograms/PrimeFactors.java
@@ -6,7 +6,7 @@ package com.java.corePrograms;
 
 import java.util.Scanner;
 
-public class Factors {
+public class PrimeFactors {
     public static void main(String args[]) {
         int number;
         Scanner sc = new Scanner(System.in);
